97c2fcc0831db375fdb00edd2579a9822ad664c9
Daniil Polyakov <arjentix@gmail.com>
2025-05-12 00:37:26 +0300

    (protocol, prover, trusted_setup, verifier): setup communication

--- prover/src/main.rs
+++ prover/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use eyre::{Context as _, Result};
+
+use network::Network;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    logger::init_logger().wrap_err("failed to initialize logger")?;
+
+    let mut network = Network::establish(
+        SocketAddr::from(([127, 0, 0, 1], 1031)),
+        [
+            SocketAddr::from(([127, 0, 0, 1], 1032)),
+            SocketAddr::from(([127, 0, 0, 1], 1033)),
+        ]
+        .as_slice(),
+    )
+    .await
+    .wrap_err("failed to establish network")?;
+
+    network
+        .broadcast(protocol::PublishCircuit {})
+        .await
+        .wrap_err("failed to broadcast `PublishCircuit` message")?;
+
+    let _trusted_setup_output: protocol::TrustedSetupOutput = network
+        .recv()
+        .await
+        .wrap_err("failed to receive `TrustedSetupOutput` message")?;
+
+    network
+        .broadcast(protocol::Proof {})
+        .await
+        .wrap_err("failed to broadcast `Proof` message")?;
+
+    Ok(())
 }
