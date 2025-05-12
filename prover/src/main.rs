66a7eabebd12e1d0ab3b300aa5dca39dc7bdd5f4
Daniil Polyakov <arjentix@gmail.com>
2025-05-12 19:48:34 +0300

    prover: add Circuit structure

--- prover/src/main.rs
+++ prover/src/main.rs
@@ -1,8 +1,11 @@
 use std::net::SocketAddr;
 
+use circuit_macro::circuit;
 use eyre::{Context as _, Result};
 
 use network::Network;
+
+use prover::{qap::Qap, r1cs::R1cs};
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -18,6 +21,62 @@ async fn main() -> Result<()> {
     )
     .await
     .wrap_err("failed to establish network")?;
+
+    // Circuit::new()
+    //     .add_constraint(
+    //         Expression::Plus {
+    //             left: Box::new(Expression::Minus {
+    //                 left: Box::new(Expression::Minus {
+    //                     left: Box::new(Expression::Plus {
+    //                         left: Box::new(Expression::Multiply {
+    //                             left: Box::new(Expression::Multiply {
+    //                                 left: Box::new(Expression::Multiply {
+    //                                     left: Box::new(Expression::Constant(3.0)),
+    //                                     right: Box::new(Expression::Variable("x")),
+    //                                 }),
+    //                                 right: Box::new(Expression::Variable("x")),
+    //                             }),
+    //                             right: Box::new(Expression::Variable("y")),
+    //                         }),
+    //                         right: Box::new(Expression::Multiply {
+    //                             left: Box::new(Expression::Multiply {
+    //                                 left: Box::new(Expression::Constant(5.0)),
+    //                                 right: Box::new(Expression::Variable("x")),
+    //                             }),
+    //                             right: Box::new(Expression::Variable("y")),
+    //                         }),
+    //                     }),
+    //                     right: Box::new(Expression::Variable("x")),
+    //                 }),
+    //                 right: Box::new(Expression::Multiply {
+    //                     left: Box::new(Expression::Constant(2.0)),
+    //                     right: Box::new(Expression::Variable("y")),
+    //                 }),
+    //             }),
+    //             right: Box::new(Expression::Constant(3.0)),
+    //         },
+    //         Expression::Variable("a"),
+    //     )
+    //     .add_constraint(
+    //         Expression::Plus {
+    //             left: Box::new(Expression::Multiply {
+    //                 left: Box::new(Expression::Constant(2.0)),
+    //                 right: Box::new(Expression::Variable("x")),
+    //             }),
+    //             right: Box::new(Expression::Variable("y")),
+    //         },
+    //         Expression::Minus {
+    //             left: Box::new(Expression::Variable("b")),
+    //             right: Box::new(Expression::Constant(5.0)),
+    //         },
+    //     );
+
+    let circuit = circuit! {
+        3*x*x*y + 5*x*y - x - 2*y + 3 == a;
+        2*x + y == b - 5;
+    };
+    let r1cs = R1cs::from(circuit);
+    let _qap = Qap::from(r1cs);
 
     network
         .broadcast(protocol::PublishCircuit {})
